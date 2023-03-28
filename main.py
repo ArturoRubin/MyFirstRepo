3bc3809e2de30467eeeae645db019e6b8a236909
ArturoRubin <116040655+ArturoRubin@users.noreply.github.com>
2023-03-28 13:37:30 -0600

    Se agregó el  programa

--- main.py
+++ main.py
@@ -3,4 +3,14 @@ def hello_world(word):
 
 print(hello_world(word="Holi uwu"))
 
+import datetime
 
+nombre = input("¿Cómo te llamas? ")
+hoy = datetime.datetime.now()
+dia_de_la_semana = hoy.strftime("%A")
+
+print("Hola " + nombre + ", ¡hoy es " + dia_de_la_semana + ", así que eres un " + dia_de_la_semana.lower() + "!")
+
+
+#Te dej o un program que te dice que dia de la semana es utilizando tu nombre
+#ARG
